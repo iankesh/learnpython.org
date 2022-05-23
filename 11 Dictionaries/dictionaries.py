764c29aae870a7fda1bedb5f950b6eec13994896
iankesh <iankesh@hotmail.com>
2022-05-23 16:17:19 +0530

    dictionaries

---
+++ 11 Dictionaries/dictionaries.py
@@ -0,0 +1,36 @@
+"""
+
+Add "Jake" to the phonebook with the phone number 938273443, and remove Jill from the phonebook.
+
+phonebook = {
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}
+# your code goes here
+
+# testing code
+if "Jake" in phonebook:
+    print("Jake is listed in the phonebook.")
+
+if "Jill" not in phonebook:
+    print("Jill is not listed in the phonebook.")
+
+"""
+
+phonebook = {
+    "John": 938477566,
+    "Jack": 938377264,
+    "Jill": 947662781
+}
+
+# your code goes here
+phonebook["Jake"] = 938273443
+del phonebook["Jill"]
+
+# testing code
+if "Jake" in phonebook:
+    print("Jake is listed in the phonebook.")
+
+if "Jill" not in phonebook:
+    print("Jill is not listed in the phonebook.")
